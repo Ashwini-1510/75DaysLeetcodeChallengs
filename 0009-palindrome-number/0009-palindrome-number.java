9611ee7d9cbe775038910424cf9fe9e41e6bc30e
Ashwini Patare <ashwinipatare1510@gmail.com>
2026-08-08 17:25:01 +0530

    Time: 5 ms (81.23%), Space: 45.8 MB (87.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+        int rev=0;
+
+        while(x>0){
+            int mod=x%10;
+             rev=(rev*10)+mod;
+             x=x/10;
+        }
+        if(num==rev){
+            return true;
+        }
+        return false;
+    }
+}
